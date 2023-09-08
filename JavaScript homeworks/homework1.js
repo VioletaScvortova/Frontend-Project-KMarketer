46cafd37c3b3777cee9829856a4cf006ef57d750
VioletaScvortova <violeta.scvortova@gmail.com>
2023-09-08 10:55:17 +0300

    added updates

---
+++ JavaScript homeworks/homework1.js
@@ -0,0 +1,11 @@
+let countryName = 'Portugal';
+let countryContinent = 'Europe';
+let countryPopulation = 10.2;
+let isIsland = false;
+let countryDescription = `${countryName} is located in ${countryContinent}, ${countryPopulation} million people live here`
+
+console.log (countryName)
+console.log (countryContinent)
+console.log (countryPopulation)
+console.log (isIsland)
+console.log (countryDescription)
