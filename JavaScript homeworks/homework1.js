fa7e39e1c87afabf1b8ffb2752334e43b48425c0
VioletaScvortova <violeta.scvortova@gmail.com>
2023-09-08 11:29:35 +0300

    Moved homework-2.js JavaScript homeworks

--- JavaScript homeworks/homework1.js
+++ JavaScript homeworks/homework1.js
@@ -4,6 +4,7 @@ let countryPopulation = 10.2;
 let isIsland = false;
 let countryDescription = `${countryName} is located in ${countryContinent}, ${countryPopulation} million people live here`
 
+
 console.log (countryName)
 console.log (countryContinent)
 console.log (countryPopulation)
