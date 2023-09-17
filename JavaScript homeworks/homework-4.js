c2255fffcbc5d0f0ad5524d47e2202aaa7a56781
VioletaScvortova <violeta.scvortova@gmail.com>
2023-09-17 22:46:10 +0300

    Added updates to variable names

--- JavaScript homeworks/homework-4.js
+++ JavaScript homeworks/homework-4.js
@@ -6,20 +6,16 @@ console.log(fruits); // (3) ['apple', 'banana', 'orange']
 
 
 //Array task 2//
-var numbers = [1, 2, 3, 4, 5];
-var removedNumber = numbers.pop();
-console.log(removedNumber); // 5
-console.log(numbers); // (4) [1, 2, 3, 4]
-
-var removedNumber = numbers.shift();
-console.log(removedNumber); // 1
-console.log(numbers); // (3) [2, 3, 4]
+let numbers = [1, 2, 3, 4, 5];
+numbers.pop();
+console.log(numbers); // (4) [1, 2, 3, 4]
+numbers.shift();
+console.log (numbers);// (3) [2, 3, 4]
 
 
 //Array task 3//
 let colours = ["red", "green", "blue", "yellow"];
-var index = colours.indexOf("green");
-console.log(index); // 1
+console.log (colours.indexOf("green")); // 1
 
 let hasOrange = colours.includes("orange");
 console.log(hasOrange); // false
@@ -30,34 +26,33 @@ let names = ["John", "Jane", "Mike", "Jennifer"];
 names.unshift("David");
 
 console.log(names); // (5) ['David', 'John', 'Jane', 'Mike', 'Jennifer']
-var index = names.indexOf("Mike");
-console.log(index); // 3
+console.log (names.indexOf("Mike")); // 3
 
 
 //Array task 5//
-var numbers = [2, 4, 6, 8, 10];
-var hasNumber = numbers.includes(5);
+let quantity = [2, 4, 6, 8, 10];
+let  hasQuantity = quantity.includes(5);
 
-console.log(hasNumber); // false 
-numbers.push(12);
-console.log(numbers); // (6) [2, 4, 6, 8, 10, 12]
+console.log(hasQuantity); // false 
+quantity.push(12);
+console.log(quantity); // (6) [2, 4, 6, 8, 10, 12]
 
 
-//Array tasks 6, 7, 8//
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//Array tasks 6, 7//
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let sort = months.sort();
+console.log(sort); // (12) ['April', 'August', 'December', 'February', 'January', 'July', 'June', 'March', 'May', 'November', 'October', 'September']
+
+let overThreeWords = months.filter((month) => month.length > 3);
+console.log(overThreeWords); // (11) ['April', 'August', 'December', 'February', 'January', 'July', 'June', 'March', 'November', 'October', 'September']
+
+
+//Array task 8//
+let calendar = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+console.log(calendar.slice(6, 11)); // (5) ['July', 'August', 'September', 'October', 'November']
+
+
+//Array task 9//
 months.sort();
-console.log(months); // (12) ['April', 'August', 'December', 'February', 'January', 'July', 'June', 'March', 'May', 'November', 'October', 'September']
-
-var result = months.filter((month) => month.length > 3);
-console.log(result); // (11) ['April', 'August', 'December', 'February', 'January', 'July', 'June', 'March', 'November', 'October', 'September']
-
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-console.log(months.slice(6,11)); // (5) ['July', 'August', 'September', 'October', 'November']
-
-
-//Array tasks 6, 7, 8, 9//
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-months.sort();
-var result = months.filter((month) => month.length >= 5);
-const showString = result.join("-");
-console.log(showString);
+let overFiveWords = months.filter((month) => month.length >= 5);
+console.log(overFiveWords.join("-")); // April-August-December-February-January-March-November-October-September
