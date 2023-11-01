5c6c907a587879c2db638eeffc7ae8a47136d695
VioletaScvortova <violeta.scvortova@gmail.com>
2023-11-01 18:16:33 +0200

    Tried to solve the 3rd task

--- homework-12.js
+++ homework-12.js
@@ -31,3 +31,17 @@ if (zipCodeIsValid) {
     console.log("The postal codes are not valid");
 }
 // Output: The postal codes are valid
+
+
+// Task 3 The result is not correct, but I tried))
+
+
+const allMovies = "1 The Shawshank Redemption (1994); 2 The Godfather (1972); 3 The Godfather: Part II (1974); 4 Pulp Fiction (1994); 5 The Good, the Bad and the Ugly (1966); 6 The Dark Knight (2008); 7 12 Angry Men (1957); 8 Schindler's List (1993); 9 The Lord of the Rings: The Return of the King (2003); 10 Fight Club (1999)";
+
+const moviesPattern = /\b\d+\s(.*?)(199[1-9]\d|200\d)\);/g;
+const moviesAfter1990 = allMovies.match(moviesPattern) || [];
+
+console.log(moviesAfter1990);
+
+
+// Task 4 I had some issues with this task
