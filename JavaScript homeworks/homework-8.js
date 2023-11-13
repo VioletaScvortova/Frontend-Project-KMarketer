3acaa3c065cd310e5fbd20d00e796d80a6f5c7c2
VioletaScvortova <violeta.scvortova@gmail.com>
2023-11-13 09:23:12 +0200

    Added the 4rd task

--- JavaScript homeworks/homework-8.js
+++ JavaScript homeworks/homework-8.js
@@ -6,7 +6,12 @@ const student = () => ({
     year: 3
 });
 
-const { firstName, lastName, faculty, year} = student();
+const {
+    firstName,
+    lastName,
+    faculty,
+    year
+} = student();
 
 const studentArray = [`${firstName} ${lastName}`, faculty, year];
 
@@ -27,6 +32,23 @@ const func = (...params) => params;
 console.log(func(1, 10, 100, 1000, 10000)); // Output: [1, 10, 100, 1000, 10000]
 
 
+// Task 3 Spread operator
+//V1 simple operation
+const countryName = ['Spain', 'Portugal', 'Norway', 'France'];
+const countryCode = [34, 351, 47, 33];
+
+const countryProp = [...countryName, ...countryCode];
+
+console.log(countryProp); // Output: ['Spain', 'Portugal', 'Norway', 'France', 34, 351, 47, 33]
 
 
+//V2 operation with function 
+const combineArrays = (arr1, arr2) => [...arr1, ...arr2];
+
+const country = ['Spain', 'Portugal', 'Norway', 'France'];
+const countryPhoneCode = [34, 351, 47, 33];
+
+const countryInfo = combineArrays(country, countryPhoneCode);
+
+console.log(countryInfo); // Output: ['Spain', 'Portugal', 'Norway', 'France', 34, 351, 47, 33]
 
