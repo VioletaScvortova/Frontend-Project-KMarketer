bfc106f98e2b08d9d327df2c1f96299cbb02ea6f
VioletaScvortova <violeta.scvortova@gmail.com>
2023-11-13 08:14:34 +0200

    Added the 2nd task

--- JavaScript homeworks/homework-8.js
+++ JavaScript homeworks/homework-8.js
@@ -1,17 +1,29 @@
-
-// Task 1
-
-  const student = () => ({
-    name: 'John',
+// Task 1 Object destructuring
+const student = () => ({
+    firstName: 'John',
     lastName: 'Doe',
     faculty: 'history',
     year: 3
-  });
-  
-  const { name, lastName, faculty, year } = student();
-  
-  const studentArray = [`${name} ${lastName}`, faculty, year];
-  
-  console.log(studentArray); // Output: ['John Doe', 'history', 3]
+});
+
+const { firstName, lastName, faculty, year} = student();
+
+const studentArray = [`${firstName} ${lastName}`, faculty, year];
+
+console.log(studentArray); // Output: ['John Doe', 'history', 3]
+
+
+// Task 2 Array destructuring
+const colors = () => ['red', 'green', 'blue', 'violet'];
+
+const [first, second, third] = colors();
+
+console.log(first); // Output: red
+console.log(third); // Output: blue
+
+
+// Task 3 Array destructuring
+
+
 
 
